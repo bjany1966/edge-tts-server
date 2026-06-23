f8094df3d59f91c6ee5978a38aa99d4a7f8054a8
bjany1966 <bjany1966@gmail.com>
2026-06-23 09:37:56 +0200

    server.py

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import Response
 import edge_tts
-import tempfile
-import wave
+import io
 
 app = FastAPI()
 
@@ -11,25 +10,15 @@ VOICE = "hu-HU-NoemiNeural"
 @app.get("/tts")
 async def tts(text: str):
 
-    # ideiglenes wav fájl
-    tmp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".wav")
+    communicate = edge_tts.Communicate(text, VOICE)
 
-    communicate = edge_tts.Communicate(text, VOICE, rate="+0%")
-
-    pcm_data = bytearray()
+    pcm = io.BytesIO()
 
     async for chunk in communicate.stream():
         if chunk["type"] == "audio":
-            pcm_data.extend(chunk["data"])
+            pcm.write(chunk["data"])
 
-    # WAV fejléc + PCM
-    with wave.open(tmp_file.name, "wb") as wf:
-        wf.setnchannels(1)
-        wf.setsampwidth(2)
-        wf.setframerate(24000)
-        wf.writeframes(pcm_data)
-
-    return FileResponse(
-        tmp_file.name,
-        media_type="audio/wav"
+    return Response(
+        content=pcm.getvalue(),
+        media_type="application/octet-stream"
     )
