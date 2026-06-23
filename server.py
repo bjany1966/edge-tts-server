e1fb1b51828b411a9bb4cfa1b25eb72ee9b7b419
bjany1966 <bjany1966@gmail.com>
2026-06-23 10:27:00 +0200

    server.py

--- server.py
+++ server.py
@@ -10,13 +10,11 @@ VOICE = "hu-HU-NoemiNeural"
 @app.get("/tts")
 async def tts(text: str):
 
-    tmp = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3")
+    tmp = tempfile.NamedTemporaryFile(delete=False, suffix=".wav")
 
     communicate = edge_tts.Communicate(text, VOICE)
 
+    # WAV = PCM container (nem MP3!)
     await communicate.save(tmp.name)
 
-    return FileResponse(
-        tmp.name,
-        media_type="audio/mpeg"
-    )
+    return FileResponse(tmp.name, media_type="audio/wav")
