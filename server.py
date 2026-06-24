91a048a0efab6a61336a3e39108dfe3d2dbca10e
bjany1966 <bjany1966@gmail.com>
2026-06-24 13:37:56 +0200

    server.py

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from openai import OpenAI
 
 key = os.environ["OPENAI_API_KEY"].strip()
 print(repr(key[:12]), len(key))
+print("KEY PREFIX:", repr(os.getenv("OPENAI_API_KEY", "")[:10]))
 
 client = OpenAI(api_key=key)
 print(client.models.list())
