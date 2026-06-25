aa08a6b98c09ecb0c920a3e047ee0a3a5a54649e
bjany1966 <bjany1966@gmail.com>
2026-06-25 13:26:41 +0200

    server.py

--- server.py
+++ server.py
@@ -26,7 +26,7 @@ async def stt(request: Request):
         if not GEMINI_API_KEY:
             return {"error": "NO API KEY SET"}
 
-        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
+        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={GEMINI_API_KEY}"
 
         payload = {
             "contents": [
